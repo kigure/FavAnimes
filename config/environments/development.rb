b504f9e018ab547521acd2d263c2e472a53558f1
kigure <kakaloto0824@gmail.com>
2022-07-13 11:52:25 +0000

    Gemの追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "303a8432cc364648afed5af4cac36256.vfs.cloud9.us-east-1.amazonaws.com"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  ##管理者
+  devise_for :admin,skip: [:passwords,:registrations], controllers: {
+  sessions: "admin/sessions"
+}
+
+  ##会員
+  devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+scope module: :public do
+  root to: "animes#top"
+end
+
 end
